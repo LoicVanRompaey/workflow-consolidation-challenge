5b48540f9b06152cf310c543ec5843a6d187ef4e
LoicVanRompaey <loic.vanrompaey@gmail.com>
2019-11-07 16:01:22 +0100

    found out how to extend constructor of parent class

--- src/Entity/Manager.php
+++ src/Entity/Manager.php
@@ -1,10 +1,26 @@
 <?php
 
+//TODO test functions wanneer alles is samengevoegd
 
 namespace App\Entity;
 
 
+use PhpParser\Node\Name;
+
 class Manager extends User
 {
 
+
+    /**
+     * Manager constructor.
+     * @param $Name
+     * @param $Email
+     * @param $password
+     */
+    public function __construct($Name, $Email, $password)
+    {
+        parent::__construct($this->setName($Name), $this->setEmail($Email), $this->setPassword($password));
+        // neemt de construct van de parent class over en gebruikt die voor zijn eigen constructor
+        $this->Role = 'Manager';
+    }
 }
